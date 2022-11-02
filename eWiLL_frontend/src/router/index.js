741a94bb7cec0a96e96450b2e11192833704c848
Medienkunzt <monoxotix@gmail.com>
2022-11-02 21:39:42 +0100

    add essencial sevices

--- eWiLL_frontend/src/router/index.js
+++ eWiLL_frontend/src/router/index.js
@@ -36,4 +36,18 @@ const router = createRouter({
   ],
 });
 
+router.beforeEach((to, from, next) => {
+  const publicPages = ["/login", "/modelling", "/checker"];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem("user");
+
+  // trying to access a restricted page + not logged in
+  // redirect to login page
+  if (authRequired && !loggedIn) {
+    next("/login");
+  } else {
+    next();
+  }
+});
+
 export default router;
