1d908c3ddb7726287eb96553c47febcde308c2b9
MaximilianMhng <maximilian.hoenig@mnd.thm.de>
2022-11-03 21:37:23 +0100

    Added ViewProfile and corresponding path

--- eWiLL_frontend/src/router/index.js
+++ eWiLL_frontend/src/router/index.js
@@ -5,6 +5,7 @@ import ViewChecker from "../views/ViewChecker.vue";
 import ViewImpressum from "../views/ViewImpressum.vue";
 import ViewDatenschutz from "../views/ViewDatenschutz.vue";
 import ViewLogin from "../views/ViewLogin.vue";
+import ViewProfile from "../views/ViewProfile.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,6 +39,11 @@ const router = createRouter({
       path: "/login",
       name: "ViewLogin",
       component: ViewLogin,
+    },
+    {
+      path: "/profile",
+      name: "ViewProfile",
+      component: ViewProfile,
     },
   ],
 });
---
+++ eWiLL_frontend/src/views/ViewProfile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Profil</h3>
+    <p>Vorname: {{ firstName }}</p>
+    <p>Nachname: {{ lastName }}</p>
+    <p>Email: {{ email }}</p>
+    <p>Rolle: {{ role }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const firstName = ref(null);
+const lastName = ref(null);
+const email = ref(null);
+const role = ref(null);
+</script>
