05389654adedd244d923d66821f172ed2cf4d930
Jonas Reitz <jonas.reitz@mni.thm.de>
2023-01-05 15:55:52 +0100

    Added basic setup vor eval

--- eWiLL_backend/src/main/kotlin/com/wipdev/eWiLL_backend/endpoints/EvaluationController.kt
+++ eWiLL_backend/src/main/kotlin/com/wipdev/eWiLL_backend/endpoints/EvaluationController.kt
@@ -1,6 +1,8 @@
 package com.wipdev.eWiLL_backend.endpoints
 
 import com.wipdev.eWiLL_backend.endpoints.payload.requests.SubmissionRequestPL
+import com.wipdev.eWiLL_backend.repository.DiagramRepository
+import com.wipdev.eWiLL_backend.repository.TaskRepository
 import com.wipdev.eWiLL_backend.services.EvaluationService
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -18,9 +20,20 @@ class EvaluationController {
     @Autowired
     lateinit var service: EvaluationService
 
+    @Autowired
+    lateinit var taskRepository: TaskRepository
+
+    @Autowired
+    lateinit var diagramRepository: DiagramRepository
+
     @PostMapping("/submit")
     @ResponseBody
     fun submit(@Parameter submissionRequestPL: SubmissionRequestPL): String? {
+        var task = taskRepository.findById(submissionRequestPL.taskId).get()
+        var solutionModel = task.solutionModelId?.let { diagramRepository.findById(it).get() }
+        val model = submissionRequestPL.diagramPL
+
+
         return service.eval(submissionRequestPL)
     }
 
--- eWiLL_backend/src/main/kotlin/com/wipdev/eWiLL_backend/endpoints/payload/requests/SubmissionRequestPL.kt
+++ eWiLL_backend/src/main/kotlin/com/wipdev/eWiLL_backend/endpoints/payload/requests/SubmissionRequestPL.kt
@@ -1,3 +1,3 @@
 package com.wipdev.eWiLL_backend.endpoints.payload.requests
 
-data class SubmissionRequestPL(val diagramPL:DiagramPL, val taskId:String)
+data class SubmissionRequestPL(val diagramPL:DiagramPL, val taskId:Long)
