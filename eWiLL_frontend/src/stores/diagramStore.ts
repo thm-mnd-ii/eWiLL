5b9dd1a9623ef4802e828f2586e9f73b65a72f91
Medienkunzt <monoxotix@gmail.com>
2023-01-06 11:04:13 +0100

    remove sample data

--- eWiLL_frontend/src/stores/diagramStore.ts
+++ eWiLL_frontend/src/stores/diagramStore.ts
@@ -18,77 +18,77 @@ const config: Config = {
   diagramType: DiagramType.SERM,
 };
 
-const entityList: Entity[] = [
-  {
-    id: 1,
-    type: EntityType.ENTITY,
-    entityName: "Kunde",
-    top: 124,
-    left: 81,
-    width: 100,
-    attributes: [
-      { name: "KNr", type: AttributeType.PrimaryKey },
-      { name: "Adresse", type: AttributeType.Attribute },
-      { name: "Name", type: AttributeType.Attribute },
-      { name: "Vorname", type: AttributeType.Attribute },
-    ],
-  },
-  {
-    id: 2,
-    type: EntityType.ENTITYRELATIONSHIP,
-    entityName: "Rechnung",
-    top: 122,
-    left: 302,
-    width: 100,
-    attributes: [
-      { name: "RNr", type: AttributeType.PrimaryKey },
-      { name: "KNr", type: AttributeType.ForeignKey },
-      { name: "Datum", type: AttributeType.Attribute },
-    ],
-  },
-  {
-    id: 3,
-    type: EntityType.ENTITY,
-    entityName: "Artikel",
-    top: 207,
-    left: 81,
-    width: 100,
-    attributes: [],
-  },
-  {
-    id: 4,
-    type: EntityType.RELATIONSHIP,
-    entityName: "Rechnungs\npositionen",
-    top: 118,
-    left: 486,
-    width: 100,
-    attributes: [],
-  },
-];
+// const entityList: Entity[] = [
+//   {
+//     id: 1,
+//     type: EntityType.ENTITY,
+//     entityName: "Kunde",
+//     top: 124,
+//     left: 81,
+//     width: 100,
+//     attributes: [
+//       { name: "KNr", type: AttributeType.PrimaryKey },
+//       { name: "Adresse", type: AttributeType.Attribute },
+//       { name: "Name", type: AttributeType.Attribute },
+//       { name: "Vorname", type: AttributeType.Attribute },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     type: EntityType.ENTITYRELATIONSHIP,
+//     entityName: "Rechnung",
+//     top: 122,
+//     left: 302,
+//     width: 100,
+//     attributes: [
+//       { name: "RNr", type: AttributeType.PrimaryKey },
+//       { name: "KNr", type: AttributeType.ForeignKey },
+//       { name: "Datum", type: AttributeType.Attribute },
+//     ],
+//   },
+//   {
+//     id: 3,
+//     type: EntityType.ENTITY,
+//     entityName: "Artikel",
+//     top: 207,
+//     left: 81,
+//     width: 100,
+//     attributes: [],
+//   },
+//   {
+//     id: 4,
+//     type: EntityType.RELATIONSHIP,
+//     entityName: "Rechnungs\npositionen",
+//     top: 118,
+//     left: 486,
+//     width: 100,
+//     attributes: [],
+//   },
+// ];
 
-const ankerPoints: Connection[] = [
-  {
-    startEntity: 1,
-    startEntityPosition: ConnectorPosition.Right,
-    endEntity: 2,
-    endEntityPosition: ConnectorPosition.Left,
-    style: CardinalityType.ONE_TO_MANY,
-  },
-  {
-    startEntity: 3,
-    startEntityPosition: ConnectorPosition.Right,
-    endEntity: 2,
-    endEntityPosition: ConnectorPosition.Left,
-    style: CardinalityType.ZERO_TO_MANY,
-  },
-  {
-    startEntity: 2,
-    startEntityPosition: ConnectorPosition.Right,
-    endEntity: 4,
-    endEntityPosition: ConnectorPosition.Left,
-    style: CardinalityType.ZERO_TO_ONE,
-  },
-];
+// const ankerPoints: Connection[] = [
+//   {
+//     startEntity: 1,
+//     startEntityPosition: ConnectorPosition.Right,
+//     endEntity: 2,
+//     endEntityPosition: ConnectorPosition.Left,
+//     style: CardinalityType.ONE_TO_MANY,
+//   },
+//   {
+//     startEntity: 3,
+//     startEntityPosition: ConnectorPosition.Right,
+//     endEntity: 2,
+//     endEntityPosition: ConnectorPosition.Left,
+//     style: CardinalityType.ZERO_TO_MANY,
+//   },
+//   {
+//     startEntity: 2,
+//     startEntityPosition: ConnectorPosition.Right,
+//     endEntity: 4,
+//     endEntityPosition: ConnectorPosition.Left,
+//     style: CardinalityType.ZERO_TO_ONE,
+//   },
+// ];
 
 export const useDiagramStore = defineStore("diagram", {
   state: (): State => {
@@ -98,8 +98,8 @@ export const useDiagramStore = defineStore("diagram", {
         ownerId: 1,
         name: "Mein Diagramm",
         config: config,
-        entities: entityList,
-        connections: ankerPoints,
+        entities: [] as Entity[],
+        connections: [] as Connection[],
       } as Diagram,
     };
   },
