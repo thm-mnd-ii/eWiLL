709129a685500fe8c0719adb63bc77b4ae2daa7b
Jonas Reitz <jonas.reitz@mni.thm.de>
2023-03-30 13:37:26 +0200

    : Added submission functionality, timestamp format, and owner role assignment to Course Management System
    Added submission functionality, timestamp format, and owner role assignment to Course Management System. Features include:
    
    Ability to add submission entries with score and result
    Function to submit work and return submission ID
    Function to retrieve submission result using ID
    Function to get available course locations
    Timestamp format added to creation date
    Automatic assignment of owner role to user when creating a new course.

--- eWiLL_backend/src/main/kotlin/com/wipdev/eWiLL_backend/endpoints/EvaluationController.kt
+++ eWiLL_backend/src/main/kotlin/com/wipdev/eWiLL_backend/endpoints/EvaluationController.kt
@@ -1,8 +1,6 @@
 package com.wipdev.eWiLL_backend.endpoints
 
 import com.wipdev.eWiLL_backend.endpoints.payload.requests.SubmissionRequestPL
-import com.wipdev.eWiLL_backend.repository.DiagramRepository
-import com.wipdev.eWiLL_backend.repository.TaskRepository
 import com.wipdev.eWiLL_backend.services.EvaluationService
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -17,20 +15,18 @@ class EvaluationController {
     @Autowired
     lateinit var service: EvaluationService
 
-    @Autowired
-    lateinit var taskRepository: TaskRepository
-
-    @Autowired
-    lateinit var diagramRepository: DiagramRepository
 
     @CrossOrigin
     @PostMapping("/submit")
     @ResponseBody
-    fun submit(@Parameter submissionRequestPL: SubmissionRequestPL): String? {
-        return service.eval(submissionRequestPL).toString()
-    }
+    fun submit(@Parameter submissionRequestPL: SubmissionRequestPL): Long? =
+        service.eval(submissionRequestPL)
 
 
+    @GetMapping("/result/{id}")
+    @ResponseBody
+    fun getResult(@PathVariable id: Long): com.wipdev.eWiLL_backend.database.tables.course.SumbissionResult? =
+        service.getEvalResult(id)
 
 
 }
