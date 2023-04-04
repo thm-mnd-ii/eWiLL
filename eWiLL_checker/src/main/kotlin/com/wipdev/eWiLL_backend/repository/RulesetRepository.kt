322446eb5ec9fc681ea4508f1449c505d35c3038
Jonas Reitz <32655811+LwTuna@users.noreply.github.com>
2023-04-04 13:55:25 +0200

    Resolves #99 (#103)

--- eWiLL_checker/src/main/kotlin/com/wipdev/eWiLL_backend/repository/RulesetRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.wipdev.eWiLL_backend.repository
-
-import com.wipdev.eWiLL_backend.database.tables.course.Ruleset
-import org.springframework.data.repository.CrudRepository
-
-interface RulesetRepository : CrudRepository<Ruleset, Long> {
-
-}
--- eWiLL_checker/src/main/kotlin/com/wipdev/eWiLL_backend/security/auth/ERole.kt
+++
@@ -1,6 +0,0 @@
-package com.wipdev.eWiLL_backend.security.auth
-
-enum class ERole {
-
-    ROLE_USER,ROLE_ADMIN
-}
