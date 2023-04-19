ce18f8e66e59a4ff8b140be48ff1bb6077dc84c7
Johannes <monoxotix@gmail.com>
2023-04-19 10:41:21 +0200

    implement new Feedback options into web

--- modules/core/web/src/services/feedback.service.ts
+++ modules/core/web/src/services/feedback.service.ts
@@ -1,14 +1,31 @@
-import axios from "axios";
+import Feedback from "@/model/Feedback";
+import axios, { AxiosResponse } from "axios";
+import authHeader from "./auth-header";
+import FeedbackStatus from "@/enums/FeedbackStatus";
 
 class FeedbackService {
-  postFeedback(feedback: string) {
-    return axios.post("/api/feedback/create", feedback, {
-      headers: { "Content-Type": "text/plain" },
+  updateFeedback(feedback: Feedback): Promise<AxiosResponse<Feedback>> {
+    return axios.post("/api/feedback/update/" + feedback.id, feedback, {
+      headers: authHeader(),
     });
   }
 
-  getFeedbacks() {
-    return axios.post("/api/feedback/get");
+  getFeedbacks(): Promise<AxiosResponse<Feedback[]>> {
+    return axios.post("/api/feedback/get", { headers: authHeader() });
+  }
+
+  createFeedback(feedback: Feedback): Promise<AxiosResponse<Feedback>> {
+    return axios.post("/api/feedback/create", feedback, {
+      headers: authHeader(),
+    });
+  }
+
+  getStatuses(): Promise<AxiosResponse<FeedbackStatus[]>> {
+    return axios.get("/api/feedback/statuses", { headers: authHeader() });
+  }
+
+  deleteFeedback(id: number) {
+    return axios.post("/api/feedback/delete/" + id, { headers: authHeader() });
   }
 }
 
--- modules/core/web/src/views/ViewFeedbackOverview.vue
+++ modules/core/web/src/views/ViewFeedbackOverview.vue
@@ -6,14 +6,25 @@
         <v-table>
           <thead>
             <tr>
-              <th class="text-left">ID</th>
+              <th class="text-left">F-ID</th>
+              <th class="text-left">Username</th>
               <th class="text-left">Feedback</th>
+              <th class="text-left">TimeStamp</th>
+              <th class="text-left">Status</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="feedback in feedbacks" :key="feedback.id">
               <td class="text-left">{{ feedback.id }}</td>
+              <td class="text-left">{{ feedback.firstName }}</td>
               <td class="text-left">{{ feedback.text }}</td>
+              <td class="text-left">{{ feedback.timeStamp }}</td>
+              <td class="text-left">
+                <v-select v-model="feedback.status" :hide-details=true variant="underlined" :items="feedbackStatuses" @update:model-value="changeStatus(feedback)"></v-select>
+              </td>
+              <td class="text-left">
+                <v-btn @click="deleteFeedback(feedback)">Delete</v-btn>
+              </td>
             </tr>
           </tbody>
         </v-table>
@@ -26,17 +37,35 @@
 import feedbackService from "../services/feedback.service";
 import Feedback from "../model/Feedback";
 import { ref, onMounted } from "vue";
+import FeedbackStatus from "@/enums/FeedbackStatus";
 
 const feedbacks = ref<Feedback[]>([]);
+const feedbackStatuses = ref<FeedbackStatus[]>();
 
 onMounted(() => {
   getFeedbacks();
+
+  feedbackService.getStatuses().then((response) => {
+    feedbackStatuses.value = response.data;
+  });
 });
 
 const getFeedbacks = () => {
   feedbackService.getFeedbacks().then((response) => {
     feedbacks.value = response.data;
     console.log(feedbacks.value);
+  });
+};
+
+const changeStatus = (feedback: Feedback) => {
+  feedbackService.updateFeedback(feedback).then((response) => {
+    console.log(response.data);
+  });
+};
+
+const deleteFeedback = (feedback: Feedback) => {
+  feedbackService.deleteFeedback(feedback.id).then((response) => {
+    console.log(response.data);
   });
 };
 </script>
@@ -51,7 +80,7 @@ const getFeedbacks = () => {
 }
 
 .card {
-  max-width: 800px;
+  width: 90vw;
   overflow-x: auto;
   margin: auto;
   padding: 20px;
