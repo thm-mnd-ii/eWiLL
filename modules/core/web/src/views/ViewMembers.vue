836bd9d408a61a88568bdcd5605f68e9ddbc9158
MaximilianMhng <maximilian.hoenig@mnd.thm.de>
2023-05-08 22:04:51 +0200

    ViewMember now using backend route

--- modules/core/web/src/views/ViewMembers.vue
+++ modules/core/web/src/views/ViewMembers.vue
@@ -5,7 +5,7 @@
       <v-card-title class="task-header-title">
         <h3 class="headline mb-0">Teilnehmer</h3>
         <v-spacer></v-spacer>
-        <v-btn variant="text" icon="mdi-keyboard-return" color="dark-gray"></v-btn>
+        <v-btn variant="text" icon="mdi-keyboard-return" color="dark-gray" @click="returnToCourse"></v-btn>
       </v-card-title>
       <v-card-text>
         <p>{{ courseName }}</p>
@@ -20,9 +20,9 @@
 
     <div class="container">
       <v-text-field v-model="search" label="Search" density="compact" prepend-icon="mdi-magnify" variant="underlined" hide-details class="search-field"></v-text-field>
-      <v-data-table :headers="headers" :items="testMembers" item-value="name" class="elevation-1" :search="search" density="default" height="480px">
+      <v-data-table :headers="headers" :items="members" item-value="name" class="elevation-1" :search="search" density="default" height="480px">
         <template #item.role="{ item }">
-          <v-select v-model="item.raw.role" item variant="plain" :items="['Student', 'Dozent', 'Owner']" class="select"></v-select>
+          <v-select v-model="item.raw.role" item variant="plain" :items="['Student', 'Dozent', 'Owner']" class="select" readonly></v-select>
         </template>
         <template #item.actions="{}">
           <v-btn icon="mdi-delete" color="red" variant="text"></v-btn>
@@ -40,12 +40,13 @@ import { useRoute, useRouter } from "vue-router";
 import { useAuthUserStore } from "../stores/authUserStore";
 import courseService from "../services/course.service";
 import CoursePL from "../model/course/CoursePL";
+import Member from "../model/course/Member";
 
 const route = useRoute();
 const router = useRouter();
 const authUserStore = useAuthUserStore();
 
-const members = ref();
+const members = ref<Member[]>();
 
 const course = ref<CoursePL>();
 const courseName = ref("");
@@ -55,9 +56,9 @@ const courseRole = ref("");
 
 const search = ref("");
 const headers = [
-  { title: "Name", align: "start", key: "name" },
-  { title: "Vorname", align: "start", key: "firstname" },
-  { title: "E-Mail", align: "start", key: "email" },
+  { title: "Name", align: "start", key: "user.lastName" },
+  { title: "Vorname", align: "start", key: "user.firstName" },
+  { title: "E-Mail", align: "start", key: "user.email" },
   { title: "Rolle", align: "start", key: "role" },
   { title: "Aktion", align: "start", key: "actions", sortable: false },
 ];
@@ -71,6 +72,7 @@ onMounted(() => {
     } else {
       courseRole.value = response;
       loadCourse();
+      loadCourseMembers();
     }
   });
 });
@@ -80,6 +82,16 @@ const loadCourse = () => {
     course.value = response.data;
     courseName.value = course.value.name;
   });
+};
+
+const loadCourseMembers = () => {
+  courseService.getCourseMembers(courseId.value).then((response) => {
+    members.value = response.data;
+  });
+};
+
+const returnToCourse = () => {
+  router.back();
 };
 </script>
 
