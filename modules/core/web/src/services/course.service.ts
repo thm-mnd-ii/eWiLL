32d747c5d78786b957cd88de548b67c7afe5af46
MaximilianMhng <maximilian.hoenig@mnd.thm.de>
2023-05-15 22:25:14 +0200

    WIP

--- modules/core/web/src/services/course.service.ts
+++ modules/core/web/src/services/course.service.ts
@@ -52,6 +52,11 @@ class CourseService{
     })
   }
 
+  changeUserRole(courseId: number){
+    //TO-DO: This route is not correct
+    return axios.post("/api/course/" + courseId + "/changeUserRole/")
+  }
+
   deleteCourse(courseId: number){
     return axios.delete("/api/course/" + courseId)
   }
