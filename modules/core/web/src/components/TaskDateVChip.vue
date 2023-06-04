106430ed852a91b16799a3bb9bb11b887b790ec8
MaximilianMhng <maximilian.hoenig@mnd.thm.de>
2023-06-04 22:09:03 +0200

    Task due date rules & v-chip with changing colors

---
+++ modules/core/web/src/components/TaskDateVChip.vue
@@ -0,0 +1,53 @@
+<!-- eslint-disable vue/valid-v-slot -->
+<template>
+  <div>
+    <v-chip v-if="timeDifference <= 0" prepend-icon="mdi-clock-time-five-outline" color="red">{{ dueDate }}</v-chip>
+    <v-chip v-if="timeDifference > 0 && timeDifference <= 7" prepend-icon="mdi-clock-time-five-outline" color="yellow">{{ dueDate }}</v-chip>
+    <v-chip v-if="timeDifference > 7" prepend-icon="mdi-clock-time-five-outline" color="green">{{ dueDate }}</v-chip>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const dueDate = ref();
+const timeDifference = ref();
+
+const setDueDate = (dateTime: string) => {
+  dueDate.value = dateTime;
+  let day = getDay(dateTime);
+  let month = getMonth(dateTime);
+  let year = getYear(dateTime);
+  let hour = getHour(dateTime);
+  let minutes = getMinutes(dateTime);
+
+  let date = new Date(year + "-" + month + "-" + day + "T" + hour + ":" + minutes + ":00");
+  let currentDate = new Date();
+
+  timeDifference.value = (date.getTime() - currentDate.getTime()) / (1000 * 3600 * 24);
+};
+
+const getDay = (dateTime: string) => {
+  return dateTime.slice(0, 2);
+};
+
+const getMonth = (dateTime: string) => {
+  return dateTime.slice(3, 5);
+};
+
+const getYear = (dateTime: string) => {
+  return dateTime.slice(6, 10);
+};
+
+const getHour = (dateTime: string) => {
+  return dateTime.slice(11, 13);
+};
+
+const getMinutes = (dateTime: string) => {
+  return dateTime.slice(14, 16);
+};
+
+defineExpose({ setDueDate });
+</script>
+
+<style scoped></style>
--- modules/core/web/src/dialog/DialogEditTask.vue
+++ modules/core/web/src/dialog/DialogEditTask.vue
@@ -10,7 +10,7 @@
             <v-select v-model="currentTask.solutionModelId" label="Musterdiagram" variant="underlined" :items="diagrams" item-title="name" item-value="id" color="#81ba24"></v-select>
           </div>
           <div>
-            <v-text-field v-model="currentTask.dueDate" label="Deadline" variant="underlined" color="#81ba24"></v-text-field>
+            <v-text-field v-model="currentTask.dueDate" :rules="dueDateRules" label="Deadline" variant="underlined" color="#81ba24" hint="DD.MM.YYYY HH:MM"></v-text-field>
             <v-select v-if="!workInProgress" v-model="currentTask.mediaType" :items="['Model', 'Text']" label="Medientyp" variant="underlined" required color="#81ba24"></v-select>
             <v-select v-if="!workInProgress" v-model="currentTask.rulesetId" :items="rulesets" item-title="name" label="Regelsatz" variant="underlined" required color="#81ba24" item-value="id"></v-select>
             <v-select v-model="currentTask.eliability" :items="liabilities" label="Verpflichtung" variant="underlined" required color="#81ba24" item-title="name" item-value="enum"></v-select>
@@ -73,6 +73,8 @@ const selectedCategoryId = ref<number>();
 const selectedDiagramId = ref<number>();
 const nameRules = ref<any>([(v: string) => !!v || "Name ist erforderlich"]);
 const descriptionRules = ref<any>([(v: string) => !!v || "Beschreibung ist erforderlich"]);
+const regex = /^([0-2][0-9]|3[0-1])\.(0[1-9]|1[0-2])\.\d{4} ([01][0-9]|2[0-3]):[0-5][0-9]$/;
+const dueDateRules = ref<any>([(v: string) => !v || regex.test(v) || "Ungültiges Datum dd.mm.yyyy hh:mm"]);
 
 // empty, or should be a valid date and in the future
 // const dueDateRules = ref<any>([(v: string) => !v || (new Date(v) > new Date() && !isNaN(new Date(v).getTime())) || "Ungültiges Datum"]);
