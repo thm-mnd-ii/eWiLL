61d33b95aae40d7f743f93aeab0c146a01e3228f
Medienkunzt <monoxotix@gmail.com>
2023-11-10 11:20:13 +0100

    fix linting

--- modules/core/web/src/dialog/DialogShowFeedback.vue
+++ modules/core/web/src/dialog/DialogShowFeedback.vue
@@ -1,6 +1,6 @@
 <template>
   <v-dialog v-model="deleteDialog" width="100vw" height="100vh">
-    <v-card   class="modelPreview" variant="outlined" width="100%" height="100%">
+    <v-card class="modelPreview" variant="outlined" width="100%" height="100%">
       <div class="message-container">
         <TaskSubmissionsResultsTabs ref="taskSubmissionsResultsTabs" class="full-height"></TaskSubmissionsResultsTabs>
       </div>
@@ -12,14 +12,11 @@
   </v-dialog>
 </template>
 
-
-
-
 <script setup lang="ts">
-  import { ref,onMounted, nextTick } from "vue";
-  import TaskSubmissionsResultsTabs from "@/components/TaskSubmissionsResultsTabs.vue";
-  import { useAuthUserStore } from "../stores/authUserStore";
-  import evaluationService from "@/services/evaluation.service";
+import { ref, onMounted, nextTick } from "vue";
+import TaskSubmissionsResultsTabs from "@/components/TaskSubmissionsResultsTabs.vue";
+import { useAuthUserStore } from "../stores/authUserStore";
+import evaluationService from "@/services/evaluation.service";
 import { useRoute } from "vue-router";
 import Task from "../model/task/Task";
 import taskService from "../services/task.service";
@@ -28,111 +25,90 @@ import Submission from "@/model/submission/Submission";
 
 const task = ref<Task>({} as Task);
 const route = useRoute();
-  const taskId = ref(Number(route.params.taskId));
-  const taskSubmissionsResultsTabs = ref<typeof TaskSubmissionsResultsTabs>();
-    const authUserStore = useAuthUserStore();
-  const userId = ref(authUserStore.auth.user?.id!);
-  const deleteDialog = ref<boolean>(false);
-  const Title = ref<string>("");
-  const Message = ref<string | undefined>(undefined);
-    const currentTask = ref<Task>({} as Task);
-  // Promises
-  const resolvePromise: any = ref(undefined);
-  const rejectPromise: any = ref(undefined);
-  const submissionCount = ref(1);
-  const submissions = ref<Submission[]>([] as Submission[]);
+const taskId = ref(Number(route.params.taskId));
+const taskSubmissionsResultsTabs = ref<typeof TaskSubmissionsResultsTabs>();
+const authUserStore = useAuthUserStore();
+const userId = ref(authUserStore.auth.user?.id!);
+const deleteDialog = ref<boolean>(false);
+const Title = ref<string>("");
+const Message = ref<string | undefined>(undefined);
+const currentTask = ref<Task>({} as Task);
+// Promises
+const resolvePromise: any = ref(undefined);
+const rejectPromise: any = ref(undefined);
+const submissionCount = ref(1);
+const submissions = ref<Submission[]>([] as Submission[]);
 
-
-
-  onMounted(() => {
+onMounted(() => {
   init();
 });
 
 const init = () => {
+  loadElements();
+};
 
- 
-      loadElements();
-    
-  }
-
-  const loadElements = async () => {
-
+const loadElements = async () => {
   await loadTask();
   await loadSubmissions();
-
-
-
-  
 };
 
 const loadTask = () => {
   taskService.getTask(taskId.value).then((response) => {
     task.value = response;
-   console
+    console;
   });
 };
 
-  const loadSubmissions = (selectedTaskIndex?: Number) => {
+const loadSubmissions = (selectedTaskIndex?: Number) => {
   evaluationService.getSubmissionIdsByUserAndTask(userId.value, taskId.value).then((response) => {
     const submissionIds = response.data;
     submissionCount.value = submissionIds.length;
     taskSubmissionsResultsTabs.value?.load(task.value, selectedTaskIndex);
   });
 };
-const openDialog =async (title: string, message: string, selectedTaskIndex?: number) => {
-
-   
-  
-   try {
+const openDialog = async (title: string, message: string, selectedTaskIndex?: number) => {
+  try {
     await loadElements();
     await loadSubmissions(selectedTaskIndex);
 
     // Force update
     await nextTick();
-
-
-  
   } catch (error) {
     console.error("Failed to load data for dialog:", error);
   }
   Title.value = title;
-  Message.value = message; 
+  Message.value = message;
   deleteDialog.value = true;
-
- 
 
   return new Promise((resolve, reject) => {
     resolvePromise.value = resolve;
     rejectPromise.value = reject;
   });
 };
-  
 
-  
-  const _cancel = () => {
-    deleteDialog.value = false;
-    resolvePromise.value(false);
-  };
-  
-  defineExpose({
-    openDialog,
-  });
-  </script>
-  
-  <style scoped>
-  .modelPreview {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background-color: white;
-    z-index: 0;
-    overflow: auto;
-    display: flex;
-    flex-direction: column;
-  }
-  .message-container {
-    flex-grow: 1;
-    display: flex;
-  }
-  </style>
-  
+const _cancel = () => {
+  deleteDialog.value = false;
+  resolvePromise.value(false);
+};
+
+defineExpose({
+  openDialog,
+});
+</script>
+
+<style scoped>
+.modelPreview {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  z-index: 0;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+}
+.message-container {
+  flex-grow: 1;
+  display: flex;
+}
+</style>
