82f5c72d5ef28df210f05cfe5d34bf45fd173eb8
Jonas Reitz <32655811+LwTuna@users.noreply.github.com>
2023-11-24 10:44:32 +0100

    Style fixes

--- modules/core/api/src/main/kotlin/com/wipdev/eWiLL_backend/database/tables/Task.kt
+++ modules/core/api/src/main/kotlin/com/wipdev/eWiLL_backend/database/tables/Task.kt
@@ -13,23 +13,40 @@ data class Task(
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     var id: Long? = null,
-    @Column(name = "name", nullable = false) var name: String? = null,
+
+    @Column(name = "name", nullable = false)
+    var name: String? = null,
+
     @Column(name = "description", nullable = false, length = 100000)
     var description: String? = null,
-    @Column(name = "dueDate", nullable = false) var dueDate: String? = null,
+
+    @Column(name = "dueDate", nullable = false)
+    var dueDate: String? = null,
+
     @Enumerated(EnumType.STRING)
     @Column(name = "mediatype", nullable = false)
     var mediaType: MediaType? = null,
+
     @Enumerated(EnumType.STRING)
     @Column(name = "liability")
     var eLiability: ELiability? = ELiability.NONE,
-    @Column(name = "courseId", nullable = false) var courseId: Long? = null,
-    @Column(name = "solutionModelId", nullable = true) var solutionModelId: Long? = null,
-    @Column(name = "rulesetId", nullable = false) var rulesetId: Long? = null,
-    @Column(name = "maxSubmissions", nullable = true) var maxSubmissions: Int? = 999,
+
+    @Column(name = "courseId", nullable = false)
+    var courseId: Long? = null,
+
+    @Column(name = "solutionModelId", nullable = true)
+    var solutionModelId: Long? = null,
+
+    @Column(name = "rulesetId", nullable = false)
+    var rulesetId: Long? = null,
+
+    @Column(name = "maxSubmissions", nullable = true)
+    var maxSubmissions: Int? = 999,
+
     @Column(name = "showLevel")
     @Enumerated(EnumType.STRING)
     var showLevel: FeedbackLevel? = FeedbackLevel.BASIC,
+    
     @Enumerated(EnumType.STRING)
     @Column(name = "level", nullable = true)
     var taskLevel: TaskLevel? = null,
