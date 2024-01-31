5df134c105ec1d563544d13dbd652d0b678a04d1
abdullah-42 <a.ural@gmail.com>
2024-01-31 19:42:08 +0100

    text commit navbar

--- modules/core/web/src/App.vue
+++ modules/core/web/src/App.vue
@@ -14,14 +14,20 @@
       </div>
     </v-app-bar>
 
-    <v-navigation-drawer v-if="$router.currentRoute.value.name != 'ViewLogin'" v-model="showSideBar" width="220" expand-on-hover rail>
+    <v-navigation-drawer v-if="$router.currentRoute.value.name != 'ViewLogin'" v-model="showSideBar" width="220"
+      expand-on-hover rail>
       <v-list density="compact" active-class="active" nav>
-        <v-list-item to="/" active-class="active" prepend-icon="mdi-human-greeting" title="Willkommen" value="introduction" />
+        <v-list-item to="/" active-class="active" prepend-icon="mdi-human-greeting" title="Willkommen"
+          value="introduction" />
         <v-list-item to="/home" active-class="active" prepend-icon="mdi-home-variant" title="Startseite" value="home" />
-        <v-list-item to="/course" active-class="active" prepend-icon="mdi-file-multiple" title="Alle Kurse" value="course" />
-        <v-list-item to="/modeling" active-class="active" prepend-icon="mdi-pencil-ruler" title="Modellierung" value="modeling" />
+        <v-list-item to="/course" active-class="active" prepend-icon="mdi-file-multiple" title="Alle Kurse"
+          value="course" />
+        <v-list-item to="/oralExam" active-class="active" prepend-icon="mdi-bug" title="Oral Examinator" value="/" />
+        <v-list-item to="/modeling" active-class="active" prepend-icon="mdi-pencil-ruler" title="Modellierung"
+          value="modeling" />
         <v-list-item to="/feedbackReport" active-class="active" prepend-icon="mdi-bug" title="Feedback" value="report" />
-        <v-list-item v-if="admin" to="/feedbackOverview" active-class="active" prepend-icon="mdi-view-dashboard-variant-outline" title="Feedback Overview" value="report" />
+        <v-list-item v-if="admin" to="/feedbackOverview" active-class="active"
+          prepend-icon="mdi-view-dashboard-variant-outline" title="Feedback Overview" value="report" />
       </v-list>
     </v-navigation-drawer>
 
@@ -32,11 +38,12 @@
 
     <v-footer v-if="$router.currentRoute.value.name != 'ViewLogin'" app absolute elevation="3">
       <v-row justify="center" no-gutters class="mt-2">
-        <v-btn v-for="link in links" :key="link.name" :to="link.url" color="gray" variant="text" class="mx-2" rounded="xl">
+        <v-btn v-for="link in links" :key="link.name" :to="link.url" color="gray" variant="text" class="mx-2"
+          rounded="xl">
           {{ link.name }}
         </v-btn>
-        <v-col class="text-center mt-4" cols="12"
-          ><strong> &copy; {{ new Date().getFullYear() }} Technische Hochschule Mittelhessen</strong>
+        <v-col class="text-center mt-4" cols="12"><strong> &copy; {{ new Date().getFullYear() }} Technische Hochschule
+            Mittelhessen</strong>
         </v-col>
       </v-row>
     </v-footer>
