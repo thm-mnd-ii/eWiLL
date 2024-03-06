9d4c11afd48c1db0b80c47ebb2f2309dccae71bd
Johannes <monoxotix@gmail.com>
2024-03-06 14:06:46 +0100

    refactor layout adjust layout for iframe integration

---
+++ modules/core/web/src/components/layout/FooterMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer v-if="$router.currentRoute.value.name != 'ViewLogin'" app absolute elevation="3">
+    <v-row justify="center" no-gutters class="mt-2">
+      <v-btn v-for="link in links" :key="link.name" :to="link.url" color="gray" variant="text" class="mx-2" rounded="xl">
+        {{ link.name }}
+      </v-btn>
+      <v-col class="text-center mt-4" cols="12"
+        ><strong> &copy; {{ new Date().getFullYear() }} Technische Hochschule Mittelhessen</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+const links = [
+  { name: 'Impressum', url: '/impressum' },
+  { name: 'Datenschutz', url: '/datenschutz' }
+]
+</script>
